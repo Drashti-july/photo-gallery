c303c5287d98dd85999ccf1be30b6e8a9ac58b49
Drashti-july <88705246+Drashti-july@users.noreply.github.com>
2021-10-24 17:18:28 +0530

    Add files via upload

---
+++ Gallery/gallery.js
@@ -0,0 +1,15 @@
+/*Name this external file gallery.js*/
+console.log("This Js is for Assingment");
+
+function upDate(previewPic) {
+  var imageSRC = previewPic.src;
+  document.getElementById("image").style.backgroundImage =
+    "url(" + imageSRC + ")";
+  document.getElementById("image").innerHTML = previewPic.alt
+}
+
+function unDo() {
+  document.getElementById("image").style.backgroundImage = "url()";
+  document.getElementById("image").innerHTML =
+    "Hover over an image below to display here.";
+}
